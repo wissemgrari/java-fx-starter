56c2e758493a61c95c9cdb4c8c43e44109a91d29
wissemgrari <wissemgrari@outlook.com>
2024-11-28 14:19:15 +0100

    scene-builder branch

--- src/main/java/com/wissem/javafxdemo/HelloApplication.java
+++ src/main/java/com/wissem/javafxdemo/HelloApplication.java
@@ -1,27 +1,24 @@
 package com.wissem.javafxdemo;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
-
+  
   public static void main(String[] args) {
     launch(args);
   }
   
-  
   @Override
   public void start(Stage stage) throws Exception {
-    
-    // Group is a type of Nodes in JavaFX ( There are various types of Root Nodes, Think of them like Layout Manager)
-    // We need at least a root node to construct the scene
-    Group root = new Group();
-    Scene scene = new Scene(root,Color.LIGHTSKYBLUE);
-    
-    stage.setScene(scene);
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    stage.setTitle("Hello World!");
+    stage.setScene(new Scene(root));
     stage.show();
   }
   
