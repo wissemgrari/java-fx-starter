b6a078dd845362aa5fc2a6d96eb71b8e669c546e
wissemgrari <wissemgrari@outlook.com>
2024-11-28 13:47:30 +0100

    stages branch

--- src/main/java/com/wissem/javafxdemo/HelloApplication.java
+++ src/main/java/com/wissem/javafxdemo/HelloApplication.java
@@ -3,6 +3,8 @@ package com.wissem.javafxdemo;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -20,6 +22,18 @@ public class HelloApplication extends Application {
     // We need at least a root node to construct the scene
     Group root = new Group();
     Scene scene = new Scene(root,Color.LIGHTSKYBLUE);
+    
+    Image icon = new Image("me.jpg");
+    stage.getIcons().add(icon);
+    stage.setTitle("Stage Demo Program w00t w00t");
+    stage.setWidth(420);
+    stage.setHeight(420);
+    stage.setResizable(false);
+    stage.setX(50);
+    stage.setY(50);
+    stage.setFullScreen(true);
+    stage.setFullScreenExitHint("YOU CAN'T ESCAPE unless you press q");
+    stage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
     
     stage.setScene(scene);
     stage.show();
