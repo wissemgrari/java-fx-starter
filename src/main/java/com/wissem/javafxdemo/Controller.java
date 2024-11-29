62b6d334a00e27d696cf831b64bfab9b21a3fca8
wissemgrari <wissemgrari@outlook.com>
2024-11-29 15:44:05 +0100

    main branch

--- src/main/java/com/wissem/javafxdemo/Controller.java
+++ src/main/java/com/wissem/javafxdemo/Controller.java
@@ -1,13 +1,5 @@
 package com.wissem.javafxdemo;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class Controller {
-  @FXML private Label welcomeText;
-  
-  @FXML
-  protected void onHelloButtonClick() {
-    welcomeText.setText("Welcome to JavaFX Application!");
-  }
+
 }
